00bb21e798cba0bdae284e79db51e882579f9544
unknown <thomas.ouellette@davidson.group>
2022-05-20 18:07:46 -0400

    feat(product-pagination): added enpoint and functionality for paginating products.

--- src/main/java/com/davidson/davcoinsapi/controller/ProductController.java
+++ src/main/java/com/davidson/davcoinsapi/controller/ProductController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -36,6 +37,11 @@ public class ProductController {
     @GetMapping
     public ResponseEntity<Iterable<Product>> getAllProducts(){
         return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/page")
+    public ResponseEntity<Iterable<Product>> getProductPage(@RequestParam int pageNumber, @RequestParam int pageSize){
+        return new ResponseEntity<>(productService.getProductPage(pageNumber, pageSize), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
--- src/main/java/com/davidson/davcoinsapi/repository/ProductRepository.java
+++ src/main/java/com/davidson/davcoinsapi/repository/ProductRepository.java
@@ -2,10 +2,13 @@ package com.davidson.davcoinsapi.repository;
 
 import com.davidson.davcoinsapi.model.Product;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends CrudRepository<Product, Long> {
-
+public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
+    Iterable<Product> findAllByOrderByNameAsc();
+    Page<Product> findAllByOrderByNameAsc(Pageable pageable);
 }
